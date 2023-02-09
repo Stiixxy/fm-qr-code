58bf46ece1cb1969e86fb1f5b7bcceb1b765904d
Stiixxy <veenman.stijn@gmail.com>
2023-02-09 21:44:55 +0100

    setup style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      lightgray: 'hsl(212, 45%, 89%)',
+      grayishblue: 'hsl(220, 15%, 55%)',
+      darkblue: 'hsl(218, 44%, 22%)',
+    },
+    fontFamily: {
+      outfit: ['Outfit'],
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
