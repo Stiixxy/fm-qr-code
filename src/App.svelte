b99dddb5d367d13e01695490fb157b713bf8eb5b
Stiixxy <veenman.stijn@gmail.com>
2023-02-09 21:45:00 +0100

    finished challange

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 </script>
 
-<main>
-  <h1 class="bg-blue-100">Hello</h1>
-</main>
+<svelte:head>
+  <title>QR Code</title>
+</svelte:head>
+
+<div class="grid h-screen place-items-center">
+  <main class="m-6 max-w-xs rounded-lg bg-white p-3 text-center">
+    <img src="image-qr-code.png" alt="qr code" class="rounded-lg" />
+    <div class="grid gap-6 p-6">
+      <h1 class="text-xl font-bold text-darkblue">
+        Improve your front-end skills by building projects
+      </h1>
+      <p class="text-sm text-grayishblue">
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  </main>
+</div>
